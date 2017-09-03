765e6f649231327049d31a87e8c285dc99e2ab2b
Bhavin Gandhi <bhavin7392@gmail.com>
2017-09-03 17:11:51 +0530

    Add some echo statements
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- docker-setup/setup.sh
+++ docker-setup/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "Downloading wordpress.tar.gz"
 curl -L http://wordpress.org/latest.tar.gz -o wordpress.tar.gz
+echo "Extracting files"
 tar -xzf wordpress.tar.gz
+echo "Changing ownership of files"
 sudo chown -R 33:33 wordpress
 rm -f wordpress.tar.gz
