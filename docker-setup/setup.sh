010d0f297435a7328901164e973e7a8154a2acf1
Bhavin Gandhi <bhavin7392@gmail.com>
2017-09-03 17:53:49 +0530

    Check if docker is installed Shows links to install docker and docker-compose if not installed on system. Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- docker-setup/setup.sh
+++ docker-setup/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+        
 echo "Downloading wordpress.tar.gz"
 curl -L http://wordpress.org/latest.tar.gz -o wordpress.tar.gz
 echo "Extracting files"
@@ -7,3 +7,13 @@ tar -xzf wordpress.tar.gz
 echo "Changing ownership of files"
 sudo chown -R 33:33 wordpress
 rm -f wordpress.tar.gz
+
+# check if docker and docker-compose is installed
+which docker &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "docker is not installed. Follow this link to install docker on your system, https://docs.docker.com/engine/installation/#server"
+fi
+which docker-compose &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "docker-compose is not installed. Follow this link to install docker-compose on your system, https://github.com/docker/compose/releases"
+fi
