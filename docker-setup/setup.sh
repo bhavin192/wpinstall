d919c3e45f5a61fbc03c354a730d6cd51604d130
Bhavin Gandhi <bhavin7392@gmail.com>
2017-09-03 23:51:55 +0530

    Create and configure wp-config.php using setup.sh Now user don't have to remember and setup database credentials.
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- docker-setup/setup.sh
+++ docker-setup/setup.sh
@@ -6,11 +6,29 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
-# download wordpress.tar.gz, extract and change ownership of files 
+# download wordpress.tar.gz, extract files 
 echo "Downloading wordpress.tar.gz"
 curl -L http://wordpress.org/latest.tar.gz -o wordpress.tar.gz
 echo "Extracting files"
 tar -xzf wordpress.tar.gz
+
+# create wp-config.php
+echo "Creating wp-config.php for WordPress site"
+sudo cp wordpress/wp-config-sample.php wordpress/wp-config.php 
+
+sudo sed -i "s/database_name_here/wordpress_db/g" wordpress/wp-config.php
+sudo sed -i "s/username_here/root/g" wordpress/wp-config.php
+sudo sed -i "s/password_here/secpass#1/g" wordpress/wp-config.php
+sudo sed -i "s/localhost/mysqldb/g" wordpress/wp-config.php
+
+salts_keys=$(curl https://api.wordpress.org/secret-key/1.1/salt) 
+salts_keys=$(echo $salts_keys | sed -e 's/\([[\/.*]\|\]\)/\\&/g')
+
+sudo sed -i "/_KEY/d" wordpress/wp-config.php
+sudo sed -i "/_SALT/d" wordpress/wp-config.php
+sudo sed -i "/define('DB_COLLATE', '');/a$salts_keys" wordpress/wp-config.php
+
+# change ownership of files
 echo "Changing ownership of files"
 sudo chown -R 33:33 wordpress
 rm -f wordpress.tar.gz
